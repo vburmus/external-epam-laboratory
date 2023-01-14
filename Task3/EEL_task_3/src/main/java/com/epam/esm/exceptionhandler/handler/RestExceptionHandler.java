50f62ca9b1a242e03eed6e219575e9ff8a0eb30b
vburmus <269545@student.pwr.edu.pl>
2023-01-14 16:57:32 +0100

    fix (EEL_task_3)  error handling

--- Task3/EEL_task_3/src/main/java/com/epam/esm/exceptionhandler/handler/RestExceptionHandler.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/exceptionhandler/handler/RestExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.epam.esm.exceptionhandler.handler;
 
 import com.epam.esm.exceptionhandler.exceptions.NoSuchItemException;
+import com.epam.esm.exceptionhandler.exceptions.ObjectAlreadyExistsException;
+import com.epam.esm.exceptionhandler.exceptions.ObjectIsInvalidException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
@@ -12,15 +14,25 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice()
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @ExceptionHandler(NoSuchItemException.class)
     public ResponseEntity<Object> handleNoSuchItemException(RuntimeException ex, WebRequest request){
-        return handleExceptionInternal(ex,ex.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND,request);
+        return handleExceptionInternal(ex,ex.getMessage(), new HttpHeaders(), HttpStatus.NO_CONTENT,request);
       }
+
+    @ResponseStatus(value = HttpStatus.CONFLICT)
+    @ExceptionHandler(ObjectAlreadyExistsException.class)
+    public ResponseEntity<Object> handleObjectAlreadyExists(RuntimeException ex, WebRequest request){
+        return handleExceptionInternal(ex,ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT,request);
+    }
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ObjectIsInvalidException.class)
+    public ResponseEntity<Object> handleObjectIsInvalid(RuntimeException ex, WebRequest request){
+        return handleExceptionInternal(ex,ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,request);
+    }
+
 
 }
