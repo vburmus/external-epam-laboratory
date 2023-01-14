ec609820d4e8f368c34501c63d1bbf52df09177f
vburmus <269545@student.pwr.edu.pl>
2023-01-14 16:57:42 +0100

    fix (EEL_task_3)  exception

--- Task3/EEL_task_3/src/main/java/com/epam/esm/exceptionhandler/exceptions/ObjectAlreadyExists.java
+++
@@ -1,7 +0,0 @@
-package com.epam.esm.exceptionhandler.exceptions;
-
-public class ObjectAlreadyExists extends AppMainException {
-    public ObjectAlreadyExists(String exception) {
-        super(exception);
-    }
-}
