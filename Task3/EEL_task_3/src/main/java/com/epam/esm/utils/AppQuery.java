88253ab22d38b72895288eecf104ace983f747a9
vburmus <269545@student.pwr.edu.pl>
2023-01-15 03:45:49 +0100

    feat (EEL_task_3)  new queries added for sorting and getting by name or part of param

--- Task3/EEL_task_3/src/main/java/com/epam/esm/utils/AppQuery.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/utils/AppQuery.java
@@ -1,7 +1,11 @@
 package com.epam.esm.utils;
 
+import com.epam.esm.taggiftcertificate.direction.DirectionEnum;
+
+import java.util.Locale;
+
 public class AppQuery {
-    public static class Tag{
+    public static class Tag {
         public static final String CREATE_TAG = "INSERT INTO tag (name) VALUES(?)";
         public static final String DELETE_TAG = "DELETE  FROM tag WHERE id=?";
         public static final String GET_ALL_TAGS = "SELECT * FROM tag";
@@ -12,7 +16,8 @@ public class AppQuery {
 
 
     }
-    public static class GiftCertificate{
+
+    public static class GiftCertificate {
         public static final String CREATE_CERTIFICATE = "INSERT INTO gift_certificate(name,description,price,duration,create_date,last_update_date) VALUES(?,?,?,?,?,?)";
         public static final String GET_ALL_CERTIFICATES = "SELECT * FROM gift_certificate";
         public static final String GET_CERTIFICATE_BY_ID = "SELECT * FROM gift_certificate WHERE id=?";
@@ -20,11 +25,24 @@ public class AppQuery {
         public static final String GET_CERTIFICATES_ID = "SELECT id FROM gift_certificate WHERE name = ? AND description = ? AND price = ? AND duration = ?";
         public static final String IS_CERTIFICATE_EXISTS = "SELECT count(*) FROM gift_certificate WHERE name = ? AND description = ? AND price = ? AND duration = ?";
         public static final String UPDATE_CERTIFICATE = "UPDATE gift_certificate SET";
-         }
-    public static class GiftCertificateHasTag{
+    }
+
+
+    public static class GiftCertificateHasTag {
         public final static String ADD_CERTIFICATE_TO_TAG = "INSERT INTO gift_certificate_has_tag (gift_certificate_id,tag_id) VALUES (?,?)";
         public final static String DELETE_CERTIFICATE_FROM_TAG = "DELETE  FROM gift_certificate_has_tag WHERE gift_certificate_id = ? AND tag_id = ?";
-        public static final String GET_ALL_TAGS_BY_CERTIFICATE_ID =  "SELECT tag.id,tag.name FROM tag,gift_certificate_has_tag where tag.id = gift_certificate_has_tag.tag_id and gift_certificate_id=?";
+        public static final String GET_ALL_TAGS_BY_CERTIFICATE_ID = "SELECT tag.id,tag.name FROM tag,gift_certificate_has_tag where tag.id = gift_certificate_has_tag.tag_id and gift_certificate_id=?";
+        public static final String GET_GIFT_CERTIFICATE_BY_TAGS_NAME = "SELECT   cert_tag.id , cert_tag.name , cert_tag.description, cert_tag.price, cert_tag.duration ,cert_tag.create_date , cert_tag.last_update_date FROM gift_cert_tagificate_has_tag cert_tagt  JOIN tag t JOIN gift_cert_tagificate cert_tag  WHERE t.id = cert_tagt.tag_id AND cert_tag.id = cert_tagt.gift_cert_tagificate_id  AND t.name = ?";
+        public static final String GET_GIFT_CERTIFICATE_BY_PART_OF_NAME = "SELECT  * FROM  gift_certificate  WHERE name LIKE ?";
+        public static final String GET_GIFT_CERTIFICATE_BY_PART_OF_DESCRIPTION = "SELECT  * FROM  gift_certificate  WHERE description LIKE ?";
+
+        public static String getSortingQueryForOneParam(DirectionEnum direction, String param) {
+            return "SELECT * FROM gift_certificate  ORDER BY " + param + " " + direction;
+        }
+
+        public static String getSortingQueryForTwoParams(DirectionEnum direction1, String param1, DirectionEnum direction2, String param2) {
+             return "SELECT * FROM gift_certificate ORDER BY " + param1 + " " + direction1 + " , " + param2 + " " +direction2;
+        }
 
     }
 }
