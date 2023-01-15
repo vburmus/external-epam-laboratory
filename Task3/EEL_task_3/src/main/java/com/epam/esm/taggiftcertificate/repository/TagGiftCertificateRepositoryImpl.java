e82c07fddb201581945d34499606c1df190051ca
vburmus <269545@student.pwr.edu.pl>
2023-01-15 03:47:55 +0100

    feat (EEL_task_3) TagGiftCertificateRepository added

---
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/taggiftcertificate/repository/TagGiftCertificateRepositoryImpl.java
@@ -0,0 +1,53 @@
+package com.epam.esm.taggiftcertificate.repository;
+
+import com.epam.esm.giftcertificate.model.GiftCertificate;
+import com.epam.esm.taggiftcertificate.direction.DirectionEnum;
+import com.epam.esm.utils.AppQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public class TagGiftCertificateRepositoryImpl implements TagGiftCertificateRepository {
+    private final String CREATE_DATE = "create_date";
+    private final String NAME = "name";
+
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public TagGiftCertificateRepositoryImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<GiftCertificate> getGiftCertificatesByTagName(String tagName) {
+        return jdbcTemplate.query(AppQuery.GiftCertificateHasTag.GET_GIFT_CERTIFICATE_BY_TAGS_NAME,new BeanPropertyRowMapper<>(GiftCertificate.class),tagName);
+    }
+
+    @Override
+    public List<GiftCertificate> getGiftCertificatesByPartOfName(String part) {
+        return jdbcTemplate.query(AppQuery.GiftCertificateHasTag.GET_GIFT_CERTIFICATE_BY_PART_OF_NAME,new BeanPropertyRowMapper<>(GiftCertificate.class),part +"%");
+    }
+    @Override
+    public List<GiftCertificate> getGiftCertificatesByPartOfDescription(String part) {
+        return jdbcTemplate.query(AppQuery.GiftCertificateHasTag.GET_GIFT_CERTIFICATE_BY_PART_OF_DESCRIPTION,new BeanPropertyRowMapper<>(GiftCertificate.class),part+"%");
+    }
+
+    @Override
+    public List<GiftCertificate> getCertificatesSortedByDate(DirectionEnum direction) {
+        return jdbcTemplate.query(AppQuery.GiftCertificateHasTag.getSortingQueryForOneParam(direction, CREATE_DATE),new BeanPropertyRowMapper<>(GiftCertificate.class));
+    }
+
+    @Override
+    public List<GiftCertificate> getCertificatesSortedByName(DirectionEnum direction) {
+        return jdbcTemplate.query(AppQuery.GiftCertificateHasTag.getSortingQueryForOneParam(direction, NAME),new BeanPropertyRowMapper<>(GiftCertificate.class));
+
+    }
+
+    @Override
+    public List<GiftCertificate> getCertificatesSortedByDateName(DirectionEnum directionDate, DirectionEnum directionName) {
+        return jdbcTemplate.query(AppQuery.GiftCertificateHasTag.getSortingQueryForTwoParams(directionDate, CREATE_DATE,directionName,NAME),new BeanPropertyRowMapper<>(GiftCertificate.class));
+
+    }
+}
