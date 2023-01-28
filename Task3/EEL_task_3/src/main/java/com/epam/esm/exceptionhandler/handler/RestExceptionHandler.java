4f92d15c264606377256c58bdba10f6bf4a272bc
vburmus <269545@student.pwr.edu.pl>
2023-01-29 00:11:32 +0100

    fix(EEL_task_3) @ResponseStatus annotation deleted as unneeded

--- Task3/EEL_task_3/src/main/java/com/epam/esm/exceptionhandler/handler/RestExceptionHandler.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/exceptionhandler/handler/RestExceptionHandler.java
@@ -17,20 +17,17 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice()
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
-    @ResponseStatus(value = HttpStatus.NO_CONTENT)
-    @ExceptionHandler(NoSuchItemException.class)
+     @ExceptionHandler(NoSuchItemException.class)
     public ResponseEntity<Object> handleNoSuchItemException(RuntimeException ex, WebRequest request) {
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.NO_CONTENT, request);
     }
 
-    @ResponseStatus(value = HttpStatus.CONFLICT)
-    @ExceptionHandler(ObjectAlreadyExistsException.class)
+     @ExceptionHandler(ObjectAlreadyExistsException.class)
     public ResponseEntity<Object> handleObjectAlreadyExists(RuntimeException ex, WebRequest request) {
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
     }
 
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(ObjectIsInvalidException.class)
+   @ExceptionHandler(ObjectIsInvalidException.class)
     public ResponseEntity<Object> handleObjectIsInvalid(RuntimeException ex, WebRequest request) {
         return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
