5f0b355d6f3f3969843a83b8be17e5eb94d0e634
vburmus <269545@student.pwr.edu.pl>
2023-03-24 13:04:23 +0100

    fix(EEL_task_3) sort method in gc and added swagger @Parameter

--- Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/controller/GiftCertificateController.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/controller/GiftCertificateController.java
@@ -4,6 +4,7 @@ import com.epam.esm.exceptionhandler.exceptions.ObjectIsInvalidException;
 import com.epam.esm.giftcertificate.direction.DirectionEnum;
 import com.epam.esm.giftcertificate.model.GiftCertificate;
 import com.epam.esm.giftcertificate.service.GiftCertificateService;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 import static com.epam.esm.utils.Constants.*;
 
@@ -19,10 +21,8 @@ import static com.epam.esm.utils.Constants.*;
 @Profile("default")
 public class GiftCertificateController {
 
-    public static final String NAME_DESC = "-name";
-    public static final String NAME_ASC = "name";
-    public static final String DATE_ASC = "date";
-    public static final String DATE_DESC = "-date";
+    public static final String NAME_REGEX = "^(-)?name$";
+    public static final String DATE_REGEX = "^(-)?date$";
     private final GiftCertificateService giftCertificateService;
 
     public GiftCertificateController(GiftCertificateService giftCertificateService) {
@@ -40,7 +40,7 @@ public class GiftCertificateController {
     }
 
     @GetMapping("/search/by-tag")
-    public ResponseEntity<?> getCertificatesByTagName(@RequestParam(NAME_ASC) String name, @RequestParam(required = false, defaultValue = DEFAULT_PAGE) Integer page, @RequestParam(required = false, defaultValue = DEFAULT_SIZE) Integer size) {
+    public ResponseEntity<?> getCertificatesByTagName(@RequestParam("name") String name, @RequestParam(required = false, defaultValue = DEFAULT_PAGE) Integer page, @RequestParam(required = false, defaultValue = DEFAULT_SIZE) Integer size) {
         return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getGiftCertificatesByTagName(name, page, size)), HttpStatus.OK);
     }
 
@@ -49,36 +49,35 @@ public class GiftCertificateController {
         return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getGiftCertificatesByPart(nameOrDescriptionPart, page, size)), HttpStatus.OK);
     }
 
+
     @GetMapping("/sort")
-    public ResponseEntity<?> getSortedCertificates(@RequestParam("sort") List<String> sort, @RequestParam(required = false, defaultValue = DEFAULT_PAGE) Integer page, @RequestParam(required = false, defaultValue = DEFAULT_SIZE) Integer size) {
+    public ResponseEntity<?> getSortedCertificates(
+            @Parameter(description = "Sorting criteria, e.g. -name,date. The '-' prefix indicates descending order. " +
+                    "The default order is ascending. The first sorting criterion is name. Example: sort=-name,date")
+            @RequestParam("sort") String sortString, @RequestParam(required = false, defaultValue = DEFAULT_PAGE) Integer page, @RequestParam(required = false, defaultValue = DEFAULT_SIZE) Integer size) {
+        String[] sort = sortString.split(",");
 
-        if (sort.size() == 1) {
-            String param = sort.get(0);
-            if (param.equals(DATE_ASC))
-                return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByDate(DirectionEnum.ASC, page, size)), HttpStatus.OK);
-            if (param.equals(DATE_DESC))
-                return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByDate(DirectionEnum.DESC, page, size)), HttpStatus.OK);
-            if (param.equals(NAME_ASC))
-                return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByName(DirectionEnum.ASC, page, size)), HttpStatus.OK);
-            if (param.equals(NAME_DESC))
-                return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByName(DirectionEnum.DESC, page, size)), HttpStatus.OK);
-        }
-        if (sort.size() == 2) {
-            if (sort.contains(DATE_ASC)) {
-                if (sort.contains(NAME_ASC))
-                    return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByDateName(DirectionEnum.ASC, DirectionEnum.ASC, page, size)), HttpStatus.OK);
-                if (sort.contains(NAME_DESC))
-                    return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByDateName(DirectionEnum.ASC, DirectionEnum.DESC, page, size)), HttpStatus.OK);
-            }
+        Pattern patternName = Pattern.compile(NAME_REGEX);
+        Pattern patternDate = Pattern.compile(DATE_REGEX);
 
-            if (sort.contains(DATE_DESC)) {
-                if (sort.contains(NAME_ASC))
-                    return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByDateName(DirectionEnum.DESC, DirectionEnum.ASC, page, size)), HttpStatus.OK);
-                if (sort.contains(NAME_DESC))
-                    return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByDateName(DirectionEnum.DESC, DirectionEnum.DESC, page, size)), HttpStatus.OK);
-            }
+        if (sort.length == 1) {
+            String param = sort[0];
+            DirectionEnum directionEnum = DirectionEnum.getDirection(param);
+            if (patternDate.matcher(param).find())
+                return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByDate(directionEnum, page, size)), HttpStatus.OK);
+            if (patternName.matcher(param).find())
+                return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByName(directionEnum, page, size)), HttpStatus.OK);
+            else throw new ObjectIsInvalidException("Wrong sort parameter provided!");
+        } else if (sort.length == 2) {
+            String name = sort[0];
+            String date = sort[1];
+            if (patternName.matcher(name).find() && patternDate.matcher(date).find()) {
+                DirectionEnum nameDirection = DirectionEnum.getDirection(name);
+                DirectionEnum dateDirection = DirectionEnum.getDirection(date);
+                return new ResponseEntity<>(Map.of(OBJECTS, giftCertificateService.getCertificatesSortedByDateName(nameDirection, dateDirection, page, size)), HttpStatus.OK);
+            } else throw new ObjectIsInvalidException("Wrong sort parameters provided!");
         }
-        throw new ObjectIsInvalidException("Please, provide correct sort parameter");
+        throw new ObjectIsInvalidException("Please, provide correct number of sort parameters sort parameters");
     }
 
     @GetMapping({"/search/by-tags"})
--- Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/direction/DirectionEnum.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/direction/DirectionEnum.java
@@ -2,5 +2,9 @@ package com.epam.esm.giftcertificate.direction;
 
 public enum DirectionEnum {
     ASC,
-    DESC
+    DESC;
+
+    public static DirectionEnum getDirection(String s){
+        return s.contains("-")?DESC:ASC;
+    }
 }
