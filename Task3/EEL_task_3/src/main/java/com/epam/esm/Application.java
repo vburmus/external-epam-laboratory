8277a71cd6ef11f2f760b067ccf570ca5284a3b4
vburmus <269545@student.pwr.edu.pl>
2023-03-27 15:21:25 +0200

    feat(EEL_task_3) JPA configured

--- Task3/EEL_task_3/src/main/java/com/epam/esm/Application.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/Application.java
@@ -3,10 +3,14 @@ package com.epam.esm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableJpaRepositories(basePackages = "com.epam.esm")
+@EntityScan(basePackages = "com.epam.esm")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
