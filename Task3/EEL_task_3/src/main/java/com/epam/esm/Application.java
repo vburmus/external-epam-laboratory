aad734408de7987238ee50a93ac0d14cbcf9acba
vburmus <269545@student.pwr.edu.pl>
2023-04-03 00:13:05 +0200

    fix(EEL_task_3) @EnableTransactionManagement and added hibernate configuration

--- Task3/EEL_task_3/src/main/java/com/epam/esm/Application.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/Application.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableJpaRepositories(basePackages = "com.epam.esm")
 @EntityScan(basePackages = "com.epam.esm")
+@EnableTransactionManagement
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
