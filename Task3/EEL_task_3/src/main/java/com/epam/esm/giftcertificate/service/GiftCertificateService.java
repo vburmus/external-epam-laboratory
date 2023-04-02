bc439457f96654e3bac09d3699f717964666e248
vburmus <269545@student.pwr.edu.pl>
2023-04-02 19:42:33 +0200

    feat(EEL_task_3) gc service refactored to jpa

--- Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/service/GiftCertificateService.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/service/GiftCertificateService.java
@@ -3,165 +3,178 @@ package com.epam.esm.giftcertificate.service;
 import com.epam.esm.exceptionhandler.exceptions.NoSuchItemException;
 import com.epam.esm.exceptionhandler.exceptions.ObjectAlreadyExistsException;
 import com.epam.esm.exceptionhandler.exceptions.ObjectIsInvalidException;
-import com.epam.esm.giftcertificate.direction.DirectionEnum;
 import com.epam.esm.giftcertificate.model.GiftCertificate;
 import com.epam.esm.giftcertificate.repository.GiftCertificateRepository;
 import com.epam.esm.tag.model.Tag;
 import com.epam.esm.tag.service.TagService;
 import com.epam.esm.utils.datavalidation.ParamsValidation;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
+import java.util.regex.Pattern;
 
 @Service
 public class GiftCertificateService {
     private final GiftCertificateRepository giftCertificateRepository;
     private final TagService tagService;
 
-    public static final String DONT_EXIST = "don't exist";
+
     public static final String IS_INVALID = " is invalid";
 
+    public static final String SORT_REGEX = "^(-)?date$|^(-)?name$";
 
-    public static final String EMPTY_MESSAGE = "Gift certificates are empty!";
 
     public GiftCertificateService(GiftCertificateRepository giftCertificateRepository, TagService tagService) {
         this.giftCertificateRepository = giftCertificateRepository;
+
         this.tagService = tagService;
     }
 
     @Transactional
     public GiftCertificate createCertificate(GiftCertificate giftCertificate) {
-        if (giftCertificateRepository.isGiftCertificateExist(giftCertificate))
+
+        Example<GiftCertificate> providedGC = Example.of(giftCertificate);
+        if (giftCertificateRepository.exists(providedGC))
             throw new ObjectAlreadyExistsException("Gift certificate with name = " + giftCertificate.getName() + ", duration = " + giftCertificate.getDuration() + " already exists");
-
         if (!ParamsValidation.isValidCertificate(giftCertificate))
-            throw new ObjectIsInvalidException("Gift certificate with name = " + giftCertificate.getName() + ", duration = " + giftCertificate.getDuration() + "is invalid, please check your params");
-
-
-        giftCertificateRepository.createGiftCertificate(giftCertificate);
-        List<Long> listTagsId = tagService.getTagsIds(giftCertificate.getTags());
-        giftCertificateRepository.createTagDependenciesForGiftCertificate(listTagsId, giftCertificateRepository.getGiftCertificatesID(giftCertificate));
-
-        return getCertificateById(giftCertificateRepository.getGiftCertificatesID(giftCertificate));
-
+            throw new ObjectIsInvalidException("Gift certificate with name = " + giftCertificate.getName() + ", duration = " + giftCertificate.getDuration() + " is invalid, please check your params");
+        giftCertificate.setCreateDate(LocalDateTime.now());
+        giftCertificate.setLastUpdateDate(LocalDateTime.now());
+        if (giftCertificate.getTags() != null) {
+            List<Tag> tags = tagService.checkTagsAndSaveIfDontExist(giftCertificate);
+            giftCertificate.setTags(tags);
+        }
+        giftCertificateRepository.save(giftCertificate);
+        return giftCertificateRepository.findById(giftCertificate.getId())
+                .orElseThrow(() -> new RuntimeException("Failed to retrieve saved GiftCertificate"));
     }
 
+    @Modifying
     public boolean deleteCertificate(long id) {
-        return giftCertificateRepository.deleteGiftCertificate(id);
+        if (giftCertificateRepository.existsById(id)) {
+            giftCertificateRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 
-    public List<GiftCertificate> getAllGiftCertificates(Integer page, Integer size) {
-        return ParamsValidation.isEmptyOrElseThrowPageException(setTagsInCertificates(giftCertificateRepository.getAllGiftCertificates(page, size)));
-    }
+    public Page<GiftCertificate> getAllGiftCertificates(Integer page, Integer size) {
 
-    @Transactional
-    public boolean updateCertificate(long id, GiftCertificate giftCertificate) {
-        Optional<Map<String, String>> updatingMap = ParamsValidation.isPatchCertificateValid(giftCertificate);
-
-        List<Tag> tagsToUpdate = giftCertificate.getTags();
-        if (updatingMap.isEmpty()) {
-            throw new ObjectIsInvalidException("Update params are empty!");
-        }
-        if (tagsToUpdate == null) return giftCertificateRepository.updateGiftCertificate(id, updatingMap.get());
-        else {
-            if (ParamsValidation.isCertificateHaveValidTags(tagsToUpdate) && giftCertificateRepository.updateGiftCertificate(id, updatingMap.get())) {
-                List<Tag> alreadyUsedTags = giftCertificateRepository.getAllTagsIdByCertificateId(id);
-                List<Tag> tagsToDelete;
-
-                List<String> tagsToUpdateNames = tagsToUpdate.stream().map(Tag::getName).collect(Collectors.toList());
-                List<String> alreadyUsedTagNames = alreadyUsedTags.stream().map(Tag::getName).toList();
-                List<String> tagsToDeleteNames = new ArrayList<>(alreadyUsedTagNames);
-                tagsToDeleteNames.removeAll(tagsToUpdateNames);
-                tagsToUpdateNames.removeAll(alreadyUsedTagNames);
-
-                if (!tagsToDeleteNames.isEmpty()) {
-                    tagsToDelete = tagService.getTagsByNames(tagsToDeleteNames);
-                    giftCertificateRepository.deleteTagDependenciesForGiftCertificate(tagsToDelete.stream().map(Tag::getId).collect(Collectors.toList()), id);
-                    alreadyUsedTags.removeAll(tagsToDelete);
-                }
-                if (!tagsToUpdateNames.isEmpty()) {
-                    tagsToUpdate = tagsToUpdate.stream().filter(tag -> tagsToUpdateNames.contains(tag.getName())).collect(Collectors.toList());
-                    tagService.isTagsExistOrElseCreate(tagsToUpdate);
-                    tagsToUpdate = tagService.getTagsByNames(tagsToUpdateNames);
-                    giftCertificateRepository.createTagDependenciesForGiftCertificate(tagsToUpdate.stream().map(Tag::getId).collect(Collectors.toList()), id);
-                }
-                return true;
-
-            }
-            throw new ObjectIsInvalidException("Some tags are invalid");
-        }
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.findAll(pageRequest));
     }
 
     public GiftCertificate getCertificateById(long id) {
 
-        GiftCertificate giftCertificate = giftCertificateRepository.getGiftCertificateByID(id);
-        if (giftCertificate == null) {
+        Optional<GiftCertificate> giftCertificate = giftCertificateRepository.findById(id);
+        if (giftCertificate.isEmpty()) {
             throw new NoSuchItemException("GiftCertificate with id = " + id + " doesn't exist");
         }
-        giftCertificate.setTags(getAllTagsByCertificate(giftCertificate));
-        return giftCertificate;
-
+        return giftCertificate.get();
     }
 
-    public List<GiftCertificate> setTagsInCertificates(List<GiftCertificate> gcs) {
-        for (GiftCertificate giftCertificate : gcs) {
-            giftCertificate.setTags(getAllTagsByCertificate(giftCertificate));
+
+    @Transactional
+    @Modifying
+    public GiftCertificate updateCertificate(long id, GiftCertificate updatedGiftCertificate) {
+
+        Optional<GiftCertificate> gc = giftCertificateRepository.findById(id);
+        if (gc.isEmpty())
+            throw new NoSuchItemException("No gift certificate with id = " + id);
+        GiftCertificate giftCertificateFromDB = gc.get();
+        if (ParamsValidation.ifCertificateNeedsAnUpdate(updatedGiftCertificate, giftCertificateFromDB)) {
+            Map<String, String> updatingMap = ParamsValidation.isPatchCertificateValid(updatedGiftCertificate);
+
+            if (updatingMap.containsKey("name"))
+                giftCertificateFromDB.setName(updatingMap.get("name"));
+            if (updatingMap.containsKey("description"))
+                giftCertificateFromDB.setName(updatingMap.get("description"));
+            if (updatingMap.containsKey("price"))
+                giftCertificateFromDB.setName(updatingMap.get("price"));
+            if (updatingMap.containsKey("duration"))
+                giftCertificateFromDB.setName(updatingMap.get("duration"));
+            giftCertificateFromDB.setLastUpdateDate(LocalDateTime.now());
+
+            List<Tag> tags = tagService.checkTagsAndSaveIfDontExist(updatedGiftCertificate);
+            giftCertificateFromDB.setTags(tags);
+
+
+            return giftCertificateRepository.save(giftCertificateFromDB);
         }
-        return gcs;
+        return giftCertificateFromDB;
     }
 
-    public List<Tag> getAllTagsByCertificate(GiftCertificate gc) {
-        return tagService.getAllTagsByCertificateID(giftCertificateRepository.getGiftCertificatesID(gc));
-    }
 
-    public List<GiftCertificate> getGiftCertificatesByTagName(String tagName, Integer page, Integer size) {
+    public Page<GiftCertificate> getGiftCertificatesByTagName(String tagName, Integer page, Integer size) {
+        page--;
         if (tagName != null && !tagName.isEmpty()) {
-            if (tagService.isTagWithNameExists(tagName)) {
-                List<GiftCertificate> gc = ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.getGiftCertificatesByTagName(tagName, page, size), "Gift Certificates with tag " + tagName + DONT_EXIST);
-                return setTagsInCertificates(gc);
+            if (tagService.existsByName(tagName)) {
+                PageRequest pageRequest = PageRequest.of(page, size);
+                return ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.findByTagsName(tagName, pageRequest));
             }
             throw new NoSuchItemException("Tag with name " + tagName + " doesn't exist");
         }
         throw new ObjectIsInvalidException("Tag name " + tagName + IS_INVALID);
     }
 
-    public List<GiftCertificate> getGiftCertificatesByPart(String part, Integer page, Integer size) {
-        if (part != null && !part.isEmpty()) {
-            List<GiftCertificate> certificates = giftCertificateRepository.getGiftCertificatesByPartOfDescription(part, page, size);
-            if (certificates.isEmpty()) {
-                List<GiftCertificate> gc = ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.getGiftCertificatesByPartOfName(part, page, size), "Gift Certificates with part of name -> " + part + DONT_EXIST);
-                return setTagsInCertificates(gc);
-            }
-            List<GiftCertificate> gc = ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(certificates, "Gift Certificates with part of description -> " + part + DONT_EXIST);
-            return setTagsInCertificates(gc);
+    public Page<GiftCertificate> getGiftCertificatesByPart(String part, Integer page, Integer size) {
+        page--;
+        if (ParamsValidation.isPartValidForSearch(part)) {
+            PageRequest pageRequest = PageRequest.of(page, size);
+            Page<GiftCertificate> certificates = giftCertificateRepository.findByNameContaining(part, pageRequest);
+            if (!certificates.isEmpty())
+                return ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(certificates);
+            return ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.findByDescriptionContaining(part, pageRequest));
         }
         throw new ObjectIsInvalidException("Part of description -> " + part + IS_INVALID);
     }
 
 
-    public List<GiftCertificate> getCertificatesSortedByDate(DirectionEnum direction, Integer page, Integer size) {
-        List<GiftCertificate> gc = ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.getCertificatesSortedByDate(direction, page, size), EMPTY_MESSAGE);
-        return setTagsInCertificates(gc);
+    public Page<GiftCertificate> getCertificatesSortedByParam(String sortString, Integer page, Integer size) {
+        page--;
+        String[] sort = sortString.split(",");
+        Pattern sortPattern = Pattern.compile(SORT_REGEX);
+        if (sort.length > 2)
+            throw new ObjectIsInvalidException("To many params for sorting");
+
+        for (String sortParam : sort) {
+            if (!sortPattern.matcher(sortParam).find())
+                throw new ObjectIsInvalidException("Some sort params are invalid.");
+        }
+
+        String firstParam = sort[0];
+        Sort.Direction firstDirection = ParamsValidation.getSortDirection(firstParam);
+        firstParam = ParamsValidation.getSortParam(firstParam);
+        if (sort.length == 1) {
+            Sort sortBy = Sort.by(firstDirection, firstParam);
+            PageRequest pageRequest = PageRequest.of(page, size, sortBy);
+            return ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.findAll(pageRequest));
+        } else {
+            String secondParam = sort[1];
+            Sort.Direction secondDirection = ParamsValidation.getSortDirection(firstParam);
+            secondParam = ParamsValidation.getSortParam(secondParam);
+            Sort sortBy = Sort.by(
+                    new Sort.Order(firstDirection, firstParam),
+                    new Sort.Order(secondDirection, secondParam));
+            PageRequest pageRequest = PageRequest.of(page, size, sortBy);
+            return ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.findAll(pageRequest));
+
+        }
+
     }
 
-
-    public List<GiftCertificate> getCertificatesSortedByName(DirectionEnum direction, Integer page, Integer size) {
-        List<GiftCertificate> gc = ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.getCertificatesSortedByName(direction, page, size), EMPTY_MESSAGE);
-        return setTagsInCertificates(gc);
-    }
-
-
-    public List<GiftCertificate> getCertificatesSortedByDateName(DirectionEnum directionDate, DirectionEnum directionName, Integer page, Integer size) {
-        List<GiftCertificate> gc = ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.getCertificatesSortedByDateName(directionDate, directionName, page, size), EMPTY_MESSAGE);
-        return setTagsInCertificates(gc);
-    }
-
-    public List<GiftCertificate> getCertificatesBySeveralTags(List<Long> tags, Integer page, Integer size) {
-        return ParamsValidation.isEmptyOrElseThrowPageException(setTagsInCertificates(giftCertificateRepository.getCertificatesBySeveralTags(tags, page, size)));
+    public Page<GiftCertificate> getCertificatesBySeveralTags(List<Long> tagsId, Integer page, Integer size) {
+        page--;
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return ParamsValidation.isCertificatesArentEmptyOrElseThrowNoSuchItem(giftCertificateRepository.findByTagsIdIn(tagsId, pageRequest));
     }
 }
