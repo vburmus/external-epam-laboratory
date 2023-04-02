b17d7248264430d1491392e292e8f007142f921f
vburmus <269545@student.pwr.edu.pl>
2023-04-02 19:43:08 +0200

    feat(EEL_task_3) tag model refactored to jpa

--- Task3/EEL_task_3/src/main/java/com/epam/esm/tag/model/Tag.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/tag/model/Tag.java
@@ -1,46 +1,36 @@
 package com.epam.esm.tag.model;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+import org.springframework.hateoas.RepresentationModel;
+
+import javax.persistence.*;
 import java.util.Objects;
 
-public class Tag {
+@Entity
+@Table(name = "tag")
+@Builder
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Tag extends RepresentationModel<Tag> {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
         Tag tag = (Tag) o;
-        return Objects.equals(id, tag.id) && Objects.equals(name, tag.name);
+        return id != null && Objects.equals(id, tag.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
-    }
-
-    @Override
-    public String toString() {
-        return "Tag{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+        return getClass().hashCode();
     }
 }
