fa0deb57b951fd49add26d58183f976f2b3bcde1
vburmus <269545@student.pwr.edu.pl>
2023-04-17 23:18:57 +0200

    feat(EEL_task_3) enabled auditing

--- Task3/EEL_task_3/src/main/java/com/epam/esm/Application.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/Application.java
@@ -4,6 +4,7 @@ package com.epam.esm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -13,8 +14,9 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableJpaRepositories(basePackages = "com.epam.esm")
 @EntityScan(basePackages = "com.epam.esm")
 @EnableTransactionManagement
+@EnableJpaAuditing
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class,args);
+        SpringApplication.run(Application.class, args);
     }
 }
