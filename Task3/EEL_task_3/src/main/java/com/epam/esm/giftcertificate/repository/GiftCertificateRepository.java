e142daf902552fce2e674d32d0b420109c742073
vburmus <269545@student.pwr.edu.pl>
2023-04-17 13:54:40 +0200

    feat(EEL_task_3) removed AppQuery.java

--- Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/repository/GiftCertificateRepository.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/repository/GiftCertificateRepository.java
@@ -4,13 +4,9 @@ import com.epam.esm.giftcertificate.model.GiftCertificate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.math.BigDecimal;
 import java.util.List;
-
-import static com.epam.esm.utils.AppQuery.GiftCertificate.GET_CERTIFICATES_PRICE;
 
 
 @Repository
@@ -22,6 +18,4 @@ public interface GiftCertificateRepository extends JpaRepository<GiftCertificate
     Page<GiftCertificate> findByTagsName(String tagName, Pageable pageable);
 
     Page<GiftCertificate> findByTagsIdIn(List<Long> tags, Pageable pageable);
-    @Query(nativeQuery = true, value = GET_CERTIFICATES_PRICE)
-    BigDecimal getPriceById(Long id);
 }
--- Task3/EEL_task_3/src/main/java/com/epam/esm/tag/repository/TagRepository.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/tag/repository/TagRepository.java
@@ -1,15 +1,20 @@
 package com.epam.esm.tag.repository;
 
 import com.epam.esm.tag.model.Tag;
-import com.epam.esm.utils.AppQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    String GET_MOST_USED_TAG = "SELECT tag.id,tag.name FROM (SELECT user_id, tag_id, COUNT(*) AS tag_count FROM gift_certificate_has_tag " +
+            "  JOIN gift_certificate_has_order ON gift_certificate_has_order.gift_certificate_id = gift_certificate_has_tag.gift_certificate_id " +
+            "  JOIN purchase ON purchase.id = gift_certificate_has_order.order_id WHERE purchase.user_id = ( SELECT user_id FROM purchase " +
+            "  ORDER BY cost DESC LIMIT 1 ) GROUP BY user_id, tag_id ORDER BY tag_count DESC LIMIT 1) subquery JOIN tag ON tag.id = subquery.tag_id;";
+
+
     boolean existsByName(String tagName);
 
-    @Query(nativeQuery = true, value = AppQuery.Tag.GET_MOST_USED_TAG)
+    @Query(nativeQuery = true, value = GET_MOST_USED_TAG)
     Tag getMostUsedTag();
 }
