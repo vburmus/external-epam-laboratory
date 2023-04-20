60efa160f33c52bb599ff1c08ea395f634d648eb
vburmus <269545@student.pwr.edu.pl>
2023-04-20 11:40:25 +0200

    fix(EEL_task_3) @EntityListeners(AuditingEntityListener.class) added on GiftCertificate

--- Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/model/GiftCertificate.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/giftcertificate/model/GiftCertificate.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.hibernate.Hibernate;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -20,6 +21,7 @@ import java.util.Objects;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class GiftCertificate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
