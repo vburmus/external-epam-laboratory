4826567baf0b0e2e152c04f8823e83ee4f56fe34
vburmus <269545@student.pwr.edu.pl>
2023-05-06 11:01:59 +0200

    fix(EEL_task_3) modified UserRepository, added method to find user by email

--- Task3/EEL_task_3/src/main/java/com/epam/esm/user/repository/UserRepository.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.epam.esm.user.repository;
 import com.epam.esm.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
