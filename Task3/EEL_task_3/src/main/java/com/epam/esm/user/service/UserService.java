dea024bfd84129abb0d0564106ad9329f67b8707
vburmus <269545@student.pwr.edu.pl>
2023-05-06 11:02:19 +0200

    fix(EEL_task_3) modified UserService, added method to load user by username(email)

--- Task3/EEL_task_3/src/main/java/com/epam/esm/user/service/UserService.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.epam.esm.user.service;
 
 import com.epam.esm.exceptionhandler.exceptions.NoSuchItemException;
+import com.epam.esm.exceptionhandler.exceptions.ObjectNotFoundException;
 import com.epam.esm.user.model.User;
 import com.epam.esm.user.model.UserDTO;
 import com.epam.esm.user.repository.UserRepository;
@@ -8,12 +9,15 @@ import com.epam.esm.utils.datavalidation.ParamsValidation;
 import com.epam.esm.utils.mappers.EntityToDtoMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
     private final EntityToDtoMapper entityToDtoMapper;
 
@@ -32,5 +36,10 @@ public class UserService {
         Optional<User> user = userRepository.findById(id);
         if (user.isEmpty()) throw new NoSuchItemException("No such user!");
         return entityToDtoMapper.toUserDTO(user.get());
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username).orElseThrow(() -> new ObjectNotFoundException("User not found"));
     }
 }
