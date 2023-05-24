75ee259a85b244f18c859f3823ff5ae2b61cea18
vburmus <269545@student.pwr.edu.pl>
2023-05-24 11:58:45 +0200

    fix(EEL_task_3) modified security config with encoders/decoders

--- Task3/EEL_task_3/src/main/java/com/epam/esm/utils/config/SecurityConfig.java
+++ Task3/EEL_task_3/src/main/java/com/epam/esm/utils/config/SecurityConfig.java
@@ -1,17 +1,31 @@
 package com.epam.esm.utils.config;
 
 import com.epam.esm.auth.tokenjwt.JwtAuthenticationFilter;
+import com.epam.esm.auth.tokenjwt.JwtToUserConverter;
 import com.epam.esm.oauth.service.CustomOauth2UserService;
 import com.epam.esm.oauth.service.CustomOidcUserService;
 import com.epam.esm.user.model.Role;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -25,17 +39,20 @@ public class SecurityConfig {
     private final AuthenticationProvider authenticationProvider;
     private final CustomOidcUserService customOidcUserService;
     private final CustomOauth2UserService customOauth2UserService;
+    private final JwtToUserConverter jwtToUserConverter;
+    private final KeyUtils keyUtils;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf()
-                .and()
+                .disable()
                 .cors()
                 .disable()
                 .authorizeHttpRequests()
                 .requestMatchers(
-                        "/",
+                        "/login",
                         "/auth/**",
                         "/v3/api-docs",
                         "/v3/api-docs/**",
@@ -62,23 +79,58 @@ public class SecurityConfig {
                         "/**"
                 )
                 .hasAuthority(Role.ADMIN.name())
-                .anyRequest()
-                .authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling()
-                .and()
-                .oauth2Login()
-                .userInfoEndpoint()
-                .userService(customOauth2UserService)
-                .oidcUserService(customOidcUserService);
+                .oauth2ResourceServer(server -> server.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtToUserConverter)))
+                .oauth2Login(login -> login.userInfoEndpoint(info -> info.userService(customOauth2UserService)
+                            .oidcUserService(customOidcUserService)));
+        /*.exceptionHandling(e -> e.authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint()).accessDeniedHandler(new
+        BearerTokenAccessDeniedHandler()))*/
+        ;
 
 
         return http.build();
 
     }
+
+    @Bean
+    @Primary
+    JwtDecoder jwtAccessTokenDecoder() {
+        return NimbusJwtDecoder.withPublicKey(keyUtils.getAccessTokenPublicKey()).build();
+    }
+
+    @Bean
+    @Primary
+    JwtEncoder jwtAccessTokenEncoder() {
+        JWK jwk = new RSAKey.Builder(keyUtils.getAccessTokenPublicKey()).privateKey(keyUtils.getAccessTokenPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    @Qualifier("jwtRefreshTokenDecoder")
+    JwtDecoder jwtRefreshTokenDecoder() {
+        return NimbusJwtDecoder.withPublicKey(keyUtils.getRefreshTokenPublicKey()).build();
+    }
+
+    @Bean
+    @Qualifier("jwtRefreshTokenDecoder")
+    JwtEncoder jwtRefreshTokenEncoder() {
+        JWK jwk = new RSAKey.Builder(keyUtils.getRefreshTokenPublicKey()).privateKey(keyUtils.getRefreshTokenPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
+    }
+
+    @Bean
+    @Qualifier("jwtRefreshTokenProvider")
+    JwtAuthenticationProvider jwtRefreshTokenAuthProvider() {
+        JwtAuthenticationProvider provider = new JwtAuthenticationProvider(jwtRefreshTokenDecoder());
+        provider.setJwtAuthenticationConverter(jwtToUserConverter);
+        return provider;
+    }
+
 }
