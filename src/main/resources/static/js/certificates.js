575c24b4df6eb8ee3d44d8786caad121aaff8094
vburmus <burmusvolod@gmail.com>
2023-08-10 16:38:43 +0200

    feat(js_task) put certificates and tags into the local storage

---
+++ src/main/resources/static/js/certificates.js
@@ -0,0 +1,451 @@
+let certificates = [
+    {
+        "id": 1,
+        "name": "Certificate 1",
+        "shortDescription": "Certificate 1 short description",
+        "longDescription": "This is the long description of certificate 1.",
+        "tags": [{
+            "id": 1,
+            "name": "Food"
+        }],
+        "price": 19.99,
+        "duration": "2023-10-31",
+        "createDate": "2023-08-02T00:00:00Z",
+        "lastUpdateDate": "2023-08-02T00:00:00Z"
+    },
+    {
+        "id": 2,
+        "name": "Certificate 2",
+        "shortDescription": "Certificate 2 short description",
+        "longDescription": "This is the long description of certificate 2.",
+        "tags": [
+            {
+                "id": 1,
+                "name": "Food"
+            },
+            {
+                "id": 2,
+                "name": "Travel"
+            }],
+        "price": 29.99,
+        "duration": "2023-11-15",
+        "createDate": "2022-08-01T00:00:00Z",
+        "lastUpdateDate": "2023-08-03T00:00:00Z"
+    },
+    {
+        "id": 3,
+        "name": "Certificate 3",
+        "shortDescription": "Certificate 3 short description",
+        "longDescription": "This is the long description of certificate 3.",
+        "tags": [{
+            "id": 3,
+            "name": "Cars"
+        }],
+        "price": 24.99,
+        "duration": "2023-12-05",
+        "createDate": "2023-08-04T00:00:00Z",
+        "lastUpdateDate": "2023-08-04T00:00:00Z"
+    },
+    {
+        "id": 4,
+        "name": "Certificate 4",
+        "shortDescription": "Test Description",
+        "longDescription": "This is the long description of certificate 4.",
+        "tags": [{
+            "id": 4,
+            "name": "Trains"
+        }],
+        "price": 34.99,
+        "duration": "2023-09-30",
+        "createDate": "2023-08-05T00:00:00Z",
+        "lastUpdateDate": "2023-08-05T00:00:00Z"
+    },
+    {
+        "id": 5,
+        "name": "Certificate 5",
+        "shortDescription": "Certificate 5 short description",
+        "longDescription": "This is the long description of certificate 5.",
+        "tags": [
+            {
+                "id": 3,
+                "name": "Cars"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            }],
+        "price": 39.99,
+        "duration": "2023-11-20",
+        "createDate": "2023-08-06T00:00:00Z",
+        "lastUpdateDate": "2023-08-06T00:00:00Z"
+    },
+    {
+        "id": 6,
+        "name": "Certificate 11",
+        "shortDescription": "Certificate 11 short description",
+        "longDescription": "This is the long description of certificate 11.",
+        "tags": [{
+            "id": 6,
+            "name": "Books"
+        }],
+        "price": 14.99,
+        "duration": "2023-10-10",
+        "createDate": "2023-08-07T00:00:00Z",
+        "lastUpdateDate": "2023-08-07T00:00:00Z"
+    },
+    {
+        "id": 7,
+        "name": "Certificate 12",
+        "shortDescription": "Certificate 12 short description",
+        "longDescription": "This is the long description of certificate 12.",
+        "tags": [{
+            "id": 7,
+            "name": "Courses"
+        }],
+        "price": 44.99,
+        "duration": "2023-12-15",
+        "createDate": "2023-08-08T00:00:00Z",
+        "lastUpdateDate": "2023-08-08T00:00:00Z"
+    },
+    {
+        "id": 8,
+        "name": "Certificate 13",
+        "shortDescription": "Certificate 13 short description",
+        "longDescription": "This is the long description of certificate 13.",
+        "tags": [
+            {
+                "id": 1,
+                "name": "Food"
+            },
+            {
+                "id": 3,
+                "name": "Cars"
+            }],
+        "price": 27.99,
+        "duration": "2023-11-10",
+        "createDate": "2023-08-09T00:00:00Z",
+        "lastUpdateDate": "2023-08-09T00:00:00Z"
+    },
+    {
+        "id": 9,
+        "name": "Certificate 14",
+        "shortDescription": "Certificate 14 short description",
+        "longDescription": "This is the long description of certificate 14.",
+        "tags": [
+            {
+                "id": 3,
+                "name": "Cars"
+            },
+            {
+                "id": 2,
+                "name": "Travel"
+            }],
+        "price": 19.99,
+        "duration": "2023-12-31",
+        "createDate": "2023-08-10T00:00:00Z",
+        "lastUpdateDate": "2023-08-10T00:00:00Z"
+    },
+    {
+        "id": 10,
+        "name": "Certificate 15",
+        "shortDescription": "Certificate 15 short description",
+        "longDescription": "This is the long description of certificate 15.",
+        "tags": [{
+            "id": 3,
+            "name": "Cars"
+        }],
+        "price": 34.99,
+        "duration": "2023-10-20",
+        "createDate": "2023-08-11T00:00:00Z",
+        "lastUpdateDate": "2023-08-11T00:00:00Z"
+    },
+    {
+        "id": 4,
+        "name": "Certificate 21",
+        "shortDescription": "Test Description",
+        "longDescription": "This is the long description of certificate 21.",
+        "tags": [{
+            "id": 3,
+            "name": "Cars"
+        }],
+        "price": 34.99,
+        "duration": "2023-09-30",
+        "createDate": "2023-08-05T00:00:00Z",
+        "lastUpdateDate": "2023-08-05T00:00:00Z"
+    },
+    {
+        "id": 5,
+        "name": "Certificate 22",
+        "shortDescription": "Certificate 22 short description",
+        "longDescription": "This is the long description of certificate 22.",
+        "tags": [
+            {
+                "id": 1,
+                "name": "Food"
+            },
+            {
+                "id": 3,
+                "name": "Cars"
+            }],
+        "price": 39.99,
+        "duration": "2023-11-20",
+        "createDate": "2023-08-06T00:00:00Z",
+        "lastUpdateDate": "2023-08-06T00:00:00Z"
+    },
+    {
+        "id": 6,
+        "name": "Certificate 23",
+        "shortDescription": "Certificate 23 short description",
+        "longDescription": "This is the long description of certificate 23.",
+        "tags": [{
+            "id": 3,
+            "name": "Cars"
+        }],
+        "price": 14.99,
+        "duration": "2023-10-10",
+        "createDate": "2023-08-07T00:00:00Z",
+        "lastUpdateDate": "2023-08-07T00:00:00Z"
+    },
+    {
+        "id": 7,
+        "name": "Certificate 24",
+        "shortDescription": "Certificate 24 short description",
+        "longDescription": "This is the long description of certificate 24.",
+        "tags": [{
+            "id": 4,
+            "name": "Trains"
+        }],
+        "price": 44.99,
+        "duration": "2023-12-15",
+        "createDate": "2023-08-08T00:00:00Z",
+        "lastUpdateDate": "2023-08-08T00:00:00Z"
+    },
+    {
+        "id": 8,
+        "name": "Certificate 25",
+        "shortDescription": "Certificate 25 short description",
+        "longDescription": "This is the long description of certificate 25.",
+        "tags": [
+            {
+                "id": 4,
+                "name": "Trains"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            }
+        ],
+        "price": 27.99,
+        "duration": "2023-11-10",
+        "createDate": "2023-08-09T00:00:00Z",
+        "lastUpdateDate": "2023-08-09T00:00:00Z"
+    },
+    {
+        "id": 9,
+        "name": "Certificate 31",
+        "shortDescription": "Certificate 31 short description",
+        "longDescription": "This is the long description of certificate 31.",
+        "tags": [
+            {
+                "id": 4,
+                "name": "Trains"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            },
+            {
+                "id": 6,
+                "name": "Books"
+            }],
+        "price": 19.99,
+        "duration": "2023-12-31",
+        "createDate": "2023-08-10T00:00:00Z",
+        "lastUpdateDate": "2023-08-10T00:00:00Z"
+    },
+    {
+        "id": 10,
+        "name": "Certificate 32",
+        "shortDescription": "Certificate 32 short description",
+        "longDescription": "This is the long description of certificate 32.",
+        "tags": [{
+            "id": 5,
+            "name": "Nature"
+        }],
+        "price": 34.99,
+        "duration": "2023-10-20",
+        "createDate": "2023-08-11T00:00:00Z",
+        "lastUpdateDate": "2023-08-11T00:00:00Z"
+    },
+    {
+        "id": 4,
+        "name": "Certificate 33",
+        "shortDescription": "Test Description",
+        "longDescription": "This is the long description of certificate 33.",
+        "tags": [
+            {
+                "id": 4,
+                "name": "Trains"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            },
+            {
+                "id": 6,
+                "name": "Books"
+            }],
+        "price": 34.99,
+        "duration": "2023-09-30",
+        "createDate": "2023-08-05T00:00:00Z",
+        "lastUpdateDate": "2023-08-05T00:00:00Z"
+    },
+    {
+        "id": 5,
+        "name": "Certificate 34",
+        "shortDescription": "Certificate 34 short description",
+        "longDescription": "This is the long description of certificate 34.",
+        "tags": [
+            {
+                "id": 1,
+                "name": "Food"
+            },
+            {
+                "id": 2,
+                "name": "Travel"
+            }],
+        "price": 39.99,
+        "duration": "2023-11-20",
+        "createDate": "2023-08-06T00:00:00Z",
+        "lastUpdateDate": "2023-08-06T00:00:00Z"
+    },
+    {
+        "id": 6,
+        "name": "Certificate 35",
+        "shortDescription": "Certificate 35 short description",
+        "longDescription": "This is the long description of certificate 35.",
+        "tags": [
+            {
+                "id": 1,
+                "name": "Food"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            }],
+        "price": 14.99,
+        "duration": "2023-10-10",
+        "createDate": "2023-08-07T00:00:00Z",
+        "lastUpdateDate": "2023-08-07T00:00:00Z"
+    },
+    {
+        "id": 7,
+        "name": "Certificate 41",
+        "shortDescription": "Certificate 41 short description",
+        "longDescription": "This is the long description of certificate 41.",
+        "tags": [
+            {
+                "id": 1,
+                "name": "Food"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            }],
+        "price": 44.99,
+        "duration": "2023-12-15",
+        "createDate": "2023-08-08T00:00:00Z",
+        "lastUpdateDate": "2023-08-08T00:00:00Z"
+    },
+    {
+        "id": 8,
+        "name": "Certificate 42",
+        "shortDescription": "Certificate 42 short description",
+        "longDescription": "This is the long description of certificate 42.",
+        "tags": [
+            {
+                "id": 1,
+                "name": "Food"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            }],
+        "price": 27.99,
+        "duration": "2023-11-10",
+        "createDate": "2023-08-09T00:00:00Z",
+        "lastUpdateDate": "2023-08-09T00:00:00Z"
+    },
+    {
+        "id": 9,
+        "name": "Certificate 43",
+        "shortDescription": "Certificate 43 short description",
+        "longDescription": "This is the long description of certificate 43.",
+        "tags": [
+            {
+                "id": 6,
+                "name": "Books"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            }],
+        "price": 19.99,
+        "duration": "2023-12-31",
+        "createDate": "2023-08-10T00:00:00Z",
+        "lastUpdateDate": "2023-08-10T00:00:00Z"
+    },
+    {
+        "id": 10,
+        "name": "Certificate 45",
+        "shortDescription": "Certificate 45 short description",
+        "longDescription": "This is the long description of certificate 45.",
+        "tags": [
+            {
+                "id": 6,
+                "name": "Books"
+            },
+            {
+                "id": 5,
+                "name": "Nature"
+            }],
+        "price": 34.99,
+        "duration": "2023-10-20",
+        "createDate": "2023-08-11T00:00:00Z",
+        "lastUpdateDate": "2023-08-11T00:00:00Z"
+    }
+]
+
+let tags = [
+    {
+        "id": 1,
+        "name": "Food"
+    },
+    {
+        "id": 2,
+        "name": "Travel"
+    },
+    {
+        "id": 3,
+        "name": "Cars"
+    },
+    {
+        "id": 4,
+        "name": "Trains"
+    },
+    {
+        "id": 5,
+        "name": "Nature"
+    },
+    {
+        "id": 6,
+        "name": "Books"
+    },
+    {
+        "id": 7,
+        "name": "Courses"
+    }
+]
+
+localStorage.setItem("certificates", JSON.stringify(certificates));
+localStorage.setItem("tags", JSON.stringify(tags));
