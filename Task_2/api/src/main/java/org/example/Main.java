5041265c6698feb2a1e943a6670d2cf5de8ec7f3
vburmus <burmusvolod@gmail.com>
2023-08-10 18:52:25 +0200

    feat(task_1) added first task

---
+++ Task_2/api/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.my.Utils;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println( Utils.isAllPositiveNumbers("12","79"));
+    }
+}
