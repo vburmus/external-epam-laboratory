90cfa71fbb8092c250a2cf29fbbbc718a630f1ec
vburmus <burmusvolod@gmail.com>
2023-08-11 12:19:50 +0200

    feat(task_3_3_1) refactored repository for REST API Advanced JPA

--- Task2/Task_2/api/src/main/java/org/example/Main.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.my.Utils;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println( Utils.isAllPositiveNumbers("12","79"));
-    }
-}
