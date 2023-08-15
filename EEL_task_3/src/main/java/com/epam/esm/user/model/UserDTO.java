b75bd6211936b144b55dffdf8b0afa38a2cc9aa2
vburmus <burmusvolod@gmail.com>
2023-08-15 15:26:52 +0200

    fix(react_task) removed unnecessary data from dto and hateoas user models

--- EEL_task_3/src/main/java/com/epam/esm/user/model/UserDTO.java
+++ EEL_task_3/src/main/java/com/epam/esm/user/model/UserDTO.java
@@ -1,6 +1,5 @@
 package com.epam.esm.user.model;
 
-import com.epam.esm.order.model.Order;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +7,6 @@ import org.hibernate.Hibernate;
 import org.springframework.hateoas.RepresentationModel;
 
 import java.util.Objects;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -19,10 +17,8 @@ public class UserDTO extends RepresentationModel<UserDTO> {
     private String surname;
     private String number;
     private String email;
-    private String password;
     private Role role;
     private String provider;
-    private Set<Order> orders;
 
     @Override
     public boolean equals(Object o) {
