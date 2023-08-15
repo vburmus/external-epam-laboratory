f698f192aa772f5e39fb0c2dccffaf292df0b31d
vburmus <burmusvolod@gmail.com>
2023-08-15 10:52:54 +0200

    feat(task_react) configured cors

--- EEL_task_3/src/main/java/com/epam/esm/utils/config/SecurityConfig.java
+++ EEL_task_3/src/main/java/com/epam/esm/utils/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -29,6 +30,12 @@ import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Collections;
+import java.util.List;
 
 
 @Configuration
@@ -42,6 +49,8 @@ public class SecurityConfig {
     private final CustomOauth2UserService customOauth2UserService;
     private final JwtToUserConverter jwtToUserConverter;
     private final KeyUtils keyUtils;
+    @Value("${allowed.origins}")
+    private String allowedOrigins;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -49,8 +58,7 @@ public class SecurityConfig {
         http
                 .csrf()
                 .disable()
-                .cors()
-                .disable()
+                .cors().and()
                 .authorizeHttpRequests()
                 .requestMatchers(
                         "/",
@@ -132,5 +140,18 @@ public class SecurityConfig {
         JwtAuthenticationProvider provider = new JwtAuthenticationProvider(jwtRefreshTokenDecoder());
         provider.setJwtAuthenticationConverter(jwtToUserConverter);
         return provider;
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList(allowedOrigins));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("Authorization"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
